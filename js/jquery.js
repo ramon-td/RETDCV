a56ea3c93b0462c51fe3b7c8232d0472e6a0090a
ramon-td <122928427+ramon-td@users.noreply.github.com>
2023-09-30 13:25:57 -0600

    test3

--- js/jquery.js
+++ js/jquery.js
@@ -5574,3 +5574,31 @@
     S
   );
 });
+const slider = document.querySelector(".slider");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const slideWidth = slider.clientWidth;
+let currentIndex = 0;
+
+function goToSlide(index) {
+  currentIndex = index;
+  const translateX = -index * slideWidth;
+  slider.style.transform = `translateX(${translateX}px)`;
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slider.children.length;
+  goToSlide(currentIndex);
+}
+
+function prevSlide() {
+  currentIndex =
+    (currentIndex - 1 + slider.children.length) % slider.children.length;
+  goToSlide(currentIndex);
+}
+
+nextBtn.addEventListener("click", nextSlide);
+prevBtn.addEventListener("click", prevSlide);
+
+// Automáticamente cambia de diapositiva cada 3 segundos
+setInterval(nextSlide, 3000);
